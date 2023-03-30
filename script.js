03f126b334f28f1498a0f11b2ec9b46d6e464725
MehdiL <larfime@gmail.com>
2023-03-30 12:31:40 +0200

    correction

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
-$(function (){
+$(function () {
 
-    $("#pays").on("click", ()=>{
+    $("#pays").on("click", () => {
 
-        $.ajax({
-            url: "all.json",
-            method: "GET",
-            dataType: "json",
-            success: (paysListe) => {
-
-                let text = `<table>`;
-    
-                paysListe.forEach((pays, indice) => {
-                    text += `<tr>`;
-                    text += `<td>${indice + 1} </td>`;
-                    text += `<td>${pays.name} </td>`;
-                    text += `<td>${pays.topLevelDomain.toString()} </td>`;
-                    text += `<td>${pays.alpha2Code} </td>`;
-                    text += `<td>${pays.callingCodes[0].toString()} </td>`;
-                    text += `<td>${pays.altSpellings[1]} </td>`;
-                    text += `<td>${pays.capital} </td>`;
-                    text += `<td>${pays.region} </td>`;
-                    text += `</tr>`;
-                });
-    
-                text += `</table>`
-    
-                document.querySelector("div").innerHTML = text;
-    
-    
+        $.ajax("https://restcountries.com/v3.1/all", {
+            success: function (tableauPays) {
+                // attend que le réseau retourne une réponse en success
+                // const tableauPays = reponse.responseJSON ; // tableau objet => collection
+                let html = "";
+                for (let pays of tableauPays) {
+                    html += `<li>${pays.name.common} - ${pays.flag} </li>`
+                }
+                $(".resultats").html(html);
+            },
+            error: function () {
+                // attend que le réseau retourne une réponse en erreur
+                $(".resultats").html("<li>pb</li>");
+            },
+            complete: function () {
+                // attend que le réseau retourne une réponse quelquelsoit
             }
+            /* url: "all.json",
+             method: "GET",
+             dataType: "json",
+             success: (paysListe) => {
+ 
+                 let text = `<table>`;
+     
+                 paysListe.forEach((pays, indice) => {
+                     text += `<tr>`;
+                     text += `<td>${indice + 1} </td>`;
+                     text += `<td>${pays.name} </td>`;
+                     text += `<td>${pays.topLevelDomain.toString()} </td>`;
+                     text += `<td>${pays.alpha2Code} </td>`;
+                     text += `<td>${pays.callingCodes[0].toString()} </td>`;
+                     text += `<td>${pays.altSpellings[1]} </td>`;
+                     text += `<td>${pays.capital} </td>`;
+                     text += `<td>${pays.region} </td>`;
+                     text += `</tr>`;
+                 });
+     
+                 text += `</table>`
+     
+                 document.querySelector("div").innerHTML = text;
+     
+     
+             }*/
         })
     })
 })
